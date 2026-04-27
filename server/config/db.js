3882f0f0085e3fbc531bbe9872a57384a8738c9d
Vishnusaini63 <vs6086764@gmail.com>
2026-04-27 17:23:10 +0530

    vvv

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "snapchat_clone"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // 🔥 IMPORTANT
+  connectTimeout: 10000      // 🔥 optional but helpful
 });
 
 db.connect((err) => {
