bad4ed3861a097dfc8d59b2983cb3e25908bb1ad
Vishnusaini63 <vs6086764@gmail.com>
2026-04-30 16:15:30 +0530

    saini

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -190,10 +190,10 @@ exports.forgotPassword = (req, res) => {
 
       const transporter = nodemailer.createTransport({
         service: "gmail",
-        auth: {
-          user: "sainivishnu2103@gmail.com",
-          pass: "fcdu lvpv zcfr rqfg" // 🔐 app password
-        }
+       auth: { 
+  user: process.env.EMAIL_USER, 
+  pass: process.env.EMAIL_PASS 
+}
       });
 
       const resetLink =
